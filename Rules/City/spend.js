659789ddcf4762e064f29ac30287b640ef1fd327
dom111 <dom111@users.noreply.github.com>
2023-01-17 21:49:50 +0000

    Update to latest version of `core-treasury`.

--- Rules/City/spend.js
+++ Rules/City/spend.js
@@ -3,19 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getRules = void 0;
 const Criterion_1 = require("@civ-clone/core-rule/Criterion");
 const Effect_1 = require("@civ-clone/core-rule/Effect");
+const Gold_1 = require("@civ-clone/base-city-yield-gold/Gold");
 const Part_1 = require("@civ-clone/core-spaceship/Part");
 const Spend_1 = require("@civ-clone/core-treasury/Rules/Spend");
+const SpendCost_1 = require("@civ-clone/core-treasury/SpendCost");
 const getRules = () => [
-    new Spend_1.default(new Criterion_1.default((cityBuild) => typeof cityBuild.building() !== 'undefined' &&
-        Object.isPrototypeOf.call(Part_1.default, cityBuild.building().item())), new Criterion_1.default((cityBuild) => cityBuild.progress().value() === 0), new Effect_1.default((cityBuild, cost) => {
-        cost.add(cityBuild.remaining() * 8);
-        return cost;
-    })),
-    new Spend_1.default(new Criterion_1.default((cityBuild) => typeof cityBuild.building() !== 'undefined' &&
-        Object.isPrototypeOf.call(Part_1.default, cityBuild.building().item())), new Criterion_1.default((cityBuild) => cityBuild.progress().value() > 0), new Effect_1.default((cityBuild, cost) => {
-        cost.add(cityBuild.remaining() * 4);
-        return cost;
-    })),
+    new Spend_1.default(new Criterion_1.default((cityBuild) => cityBuild.building() !== null &&
+        Object.isPrototypeOf.call(Part_1.default, cityBuild.building().item())), new Criterion_1.default((cityBuild) => cityBuild.progress().value() === 0), new Effect_1.default((cityBuild) => new SpendCost_1.default(Gold_1.default, cityBuild.remaining() * 8))),
+    new Spend_1.default(new Criterion_1.default((cityBuild) => cityBuild.building() !== null &&
+        Object.isPrototypeOf.call(Part_1.default, cityBuild.building().item())), new Criterion_1.default((cityBuild) => cityBuild.progress().value() > 0), new Effect_1.default((cityBuild) => new SpendCost_1.default(Gold_1.default, cityBuild.remaining() * 4))),
 ];
 exports.getRules = getRules;
 exports.default = exports.getRules;
--- Rules/City/spend.ts
+++ Rules/City/spend.ts
@@ -1,41 +1,40 @@
 import CityBuild from '@civ-clone/core-city-build/CityBuild';
 import Criterion from '@civ-clone/core-rule/Criterion';
 import Effect from '@civ-clone/core-rule/Effect';
+import Gold from '@civ-clone/base-city-yield-gold/Gold';
 import Part from '@civ-clone/core-spaceship/Part';
 import Spend from '@civ-clone/core-treasury/Rules/Spend';
-import Yield from '@civ-clone/core-yield/Yield';
+import SpendCost from '@civ-clone/core-treasury/SpendCost';
 
 export const getRules: () => Spend[] = (): Spend[] => [
   new Spend(
     new Criterion(
       (cityBuild: CityBuild): boolean =>
-        typeof cityBuild.building() !== 'undefined' &&
+        cityBuild.building() !== null &&
         Object.isPrototypeOf.call(Part, cityBuild.building()!.item())
     ),
     new Criterion(
       (cityBuild: CityBuild): boolean => cityBuild.progress().value() === 0
     ),
-    new Effect((cityBuild: CityBuild, cost: Yield): Yield => {
-      cost.add(cityBuild.remaining() * 8);
-
-      return cost;
-    })
+    new Effect(
+      (cityBuild: CityBuild): SpendCost =>
+        new SpendCost(Gold, cityBuild.remaining() * 8)
+    )
   ),
 
   new Spend(
     new Criterion(
       (cityBuild: CityBuild): boolean =>
-        typeof cityBuild.building() !== 'undefined' &&
+        cityBuild.building() !== null &&
         Object.isPrototypeOf.call(Part, cityBuild.building()!.item())
     ),
     new Criterion(
       (cityBuild: CityBuild): boolean => cityBuild.progress().value() > 0
     ),
-    new Effect((cityBuild: CityBuild, cost: Yield): Yield => {
-      cost.add(cityBuild.remaining() * 4);
-
-      return cost;
-    })
+    new Effect(
+      (cityBuild: CityBuild): SpendCost =>
+        new SpendCost(Gold, cityBuild.remaining() * 4)
+    )
   ),
 ];
 
